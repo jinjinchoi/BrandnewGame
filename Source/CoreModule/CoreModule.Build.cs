b15ed6ce932900c87859a51a9b8ef030839b4e21
JinWoo <cheong546@gmail.com>
2025-08-21 16:12:41 +0900

    deps: 코어 모듈에도 GAS 모듈 의존성 추가

--- Source/CoreModule/CoreModule.Build.cs
+++ Source/CoreModule/CoreModule.Build.cs
@@ -21,7 +21,8 @@ public class CoreModule : ModuleRules
                 "Engine",
                 "Slate",
                 "SlateCore",
-                "EnhancedInput"
+                "EnhancedInput",
+                "GameplayAbilities"
             }
         );
     }
