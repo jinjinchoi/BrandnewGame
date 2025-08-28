fce69602806fbbf26fa26d5b295b18d3f8177101
JinWoo <cheong546@gmail.com>
2025-08-28 16:57:57 +0900

    feat: 서버와 클라이언트 구분하여 메세지 작성하는 기능 추가

--- Source/CoreModule/Public/DebugHelper.h
+++ Source/CoreModule/Public/DebugHelper.h
@@ -10,4 +10,28 @@ namespace DebugHelper
 			UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 		}
 	}
+
+	static void Print(const UObject* WorldContext, const FString& Message, const FColor& Color = FColor::MakeRandomColor(), const float Time = 7.f, const int32 InKey = -1)
+	{
+		if (!WorldContext || !GEngine) return;
+
+		const UWorld* World = WorldContext->GetWorld();
+		if (!World) return;
+
+		FString Prefix;
+		if (World->GetNetMode() == NM_DedicatedServer || World->GetNetMode() == NM_ListenServer)
+		{
+			Prefix = TEXT("[Server] ");
+		}
+		else
+		{
+			Prefix = TEXT("[Client] ");
+		}
+
+		const FString FinalMessage = Prefix + Message;
+
+		GEngine->AddOnScreenDebugMessage(InKey, Time, Color, FinalMessage);
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMessage);
+	}
+	
 }
