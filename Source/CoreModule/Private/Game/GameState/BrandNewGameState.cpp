160a1b02e6b39ec091c981cda5583e9357a0c095
JinWoo <cheong546@gmail.com>
2025-10-07 12:50:47 +0900

    feat: Create Game State
    전체 플레이어 정보 가져와서 Overlay에 보여줘야함

---
+++ Source/CoreModule/Private/Game/GameState/BrandNewGameState.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/GameState/BrandNewGameState.h"
+#include "GameFramework/PlayerState.h"
+
+TArray<APawn*> ABrandNewGameState::GetTeammates()
+{
+	TArray<APawn*> Result;
+	
+	for (const APlayerState* PS : PlayerArray)
+	{
+		if (PS && PS->GetPawn())
+		{
+			Result.Add(PS->GetPawn());
+		}
+	}
+
+	return Result;
+}
---
+++ Source/CoreModule/Public/Game/GameState/BrandNewGameState.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "BrandNewGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREMODULE_API ABrandNewGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	TArray<APawn*> GetTeammates();
+	
+};
